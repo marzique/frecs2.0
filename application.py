3fc8fe78bd3a8c2af9a3a75979ff1d0f391b5b44
Dennis <marzique@gmail.com>
2018-08-21 17:02:37 +0300

    work in progress

--- application.py
+++ application.py
@@ -1,15 +1,40 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
 # Initialize extension with your app.
 app.config['SECRET_KEY'] = '55500b7625c92cd318daa58fabc00e16'
 app.config['DEBUG'] = True
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 last_news_added = "99 Серпня 2020"
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+	
+	
 articles = [
 	{'Author': 'Denys Tarnavskyi',
 	 'Title': 'Title Post1',
@@ -52,7 +77,7 @@ def history():
 
 @app.route('/about')
 def about():
-	return render_template('news.html', articles=articles, title="Про Нас")
+	return render_template('news_old.html', articles=articles, title="Про Нас")
 
 
 @app.route('/register', methods=['GET', 'POST'])
