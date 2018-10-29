eb224873557dcffd404832275af2c4cb4ddacb1e
Danny Tarn <marzique@marzique.local>
2018-10-29 17:08:23 +0200

    separate config for local/heroku

--- rpd_site/__init__.py
+++ rpd_site/__init__.py
@@ -9,6 +9,13 @@ from rpd_site.constants import VAR_APP_SECRET_KEY, VAR_DEBUG, VAR_CFG_FILE
 
 app = Flask(__name__)
 
+# check if heroku env
+on_heroku = False
+if 'IS_HEROKU' in environ:
+	on_heroku = True
+	app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
+	app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
+
 # Initialize extension with your app.
 app.config.from_pyfile(VAR_CFG_FILE)
 app.config['SECRET_KEY'] = VAR_APP_SECRET_KEY
@@ -16,8 +23,17 @@ app.config['DEBUG'] = VAR_DEBUG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcPZ3UUAAAAAO_WGbcXUI9tsI-Ya8Sq89mePlAW'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcPZ3UUAAAAAFN3SdsrsEAk9xsslHQHW_byLYmg'
-app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# recaptcha disable for offline/development
+app.config['RECAPTCHA_DISABLE'] = True
+
+
+
 
 
 mail = Mail(app)
