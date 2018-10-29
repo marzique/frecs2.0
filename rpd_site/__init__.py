5debcbb4751f6b2b4d0bfc3a8904af0c98f83654
Danny Tarn <marzique@marzique.local>
2018-10-29 16:54:24 +0200

    mail test

--- rpd_site/__init__.py
+++ rpd_site/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from os import environ
 
 from rpd_site.constants import VAR_APP_SECRET_KEY, VAR_DEBUG, VAR_CFG_FILE
 
@@ -15,6 +16,9 @@ app.config['DEBUG'] = VAR_DEBUG
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6LcPZ3UUAAAAAO_WGbcXUI9tsI-Ya8Sq89mePlAW'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LcPZ3UUAAAAAFN3SdsrsEAk9xsslHQHW_byLYmg'
+app.config['MAIL_USERNAME'] = environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = environ.get('MAIL_PASSWORD')
+
 
 mail = Mail(app)
 db = SQLAlchemy(app)
