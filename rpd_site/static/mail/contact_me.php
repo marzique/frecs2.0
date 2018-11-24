902be26c2d6675a44da8d10420fdd0b9d2d7caaa
Dennis <marzique@gmail.com>
2018-11-24 12:49:01 +0200

    php contact form fill

--- rpd_site/static/mail/contact_me.php
+++ rpd_site/static/mail/contact_me.php
@@ -16,11 +16,11 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
    
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
+$to = 'marzique@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Повідомлення з успішно заповненої форми на сайті кафедри:  $name";
+$email_body = "Нове повідомлення.\n\n"."Деталі:\n\nІм'я: $name\n\nEmail: $email_address\n\nТелефон: $phone\n\nПовідомлення:\n$message";
+$headers = "Від: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Переслати: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
 ?>
